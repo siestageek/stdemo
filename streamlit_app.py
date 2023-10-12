a63c447c3cdaf730d011b3d0beaaf88a86cfdfe0
siestageek <siestageek@gmail.com>
2023-10-12 14:56:13 +0900

    streamlit main app upload

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# 멀티 페이지용 제목
+st.set_page_config(page_title='Hello, Streamlit!',
+                   page_icon='🎉')
+st.sidebar.header('Hello, Streamlit! 🎉')
+
+st.write('Welcome to Streamlit!!')
